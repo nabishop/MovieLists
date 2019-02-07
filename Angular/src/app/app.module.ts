679b4c7776c34a5849d2342aa9deecb0a1e31309
nabishop <35745821+nabishop@users.noreply.github.com>
2019-02-06 17:44:21 -0800

    Sign up form html/css complete

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
@@ -10,7 +11,8 @@ import { SignUpComponent } from './sign-up/sign-up.component';
     SignUpComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
