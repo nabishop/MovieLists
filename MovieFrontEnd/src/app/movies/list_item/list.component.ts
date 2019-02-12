aa4f6884e3cd3ca130aa680e166b1fc884f8d524
nabishop <35745821+nabishop@users.noreply.github.com>
2019-02-11 21:05:25 -0800

    lists sort alphabetically now

--- MovieFrontEnd/src/app/movies/list_item/list.component.ts
+++ MovieFrontEnd/src/app/movies/list_item/list.component.ts
@@ -4,6 +4,7 @@ import { UserComponent } from 'src/app/user/user.component';
 import { LoginService } from 'src/app/shared/login.service';
 import { ToastrService } from 'ngx-toastr';
 import { Subscription } from 'rxjs';
+import { ListItem } from './listitem';
 
 @Component({
   selector: 'app-movies-listitem',
@@ -12,7 +13,7 @@ import { Subscription } from 'rxjs';
 })
 export class ListComponent implements OnInit {
   listsSubscription: Subscription;
-  lists: any[];
+  lists: ListItem[];
 
   constructor(private listService: ListService, private loginService: LoginService, private toastr: ToastrService) { }
 
@@ -28,8 +29,11 @@ export class ListComponent implements OnInit {
       // user is found in db
       if (resp.status.toString() == '200') {
         // error: passwords not the same but right user
-        this.listService.lists = resp.body;
-        this.lists = this.listService.lists;
+        this.lists = resp.body;
+        console.log(this.lists);
+        this.lists.sort(this.sortListByDateAdded);
+        console.log(this.lists);
+
       }
       else {
         this.toastr.error('Error retriving lists', 'List retrieval failed.');
@@ -43,5 +47,11 @@ export class ListComponent implements OnInit {
       // reload lists
       this.getListsOfUser();
     });
+  }
+
+  sortListByDateAdded(one: ListItem, two: ListItem) {
+    if (one.dateAdded > two.dateAdded) return -1;
+    else if (one.dateAdded < two.dateAdded) return 1;
+    return 0;
   }
 }
---
+++ MovieFrontEnd/src/app/movies/list_item/listitem.ts
@@ -0,0 +1,6 @@
+export interface ListItem {
+    "name": string,
+    "dateAdded": string,
+    "movie_id": number,
+    "user_id": number
+}
